24408b42c2e0f309fbc12bdf93bd250c1876b9b6
Arjay Jones <rj.javadev@icloud.com>
2019-01-14 09:59:11 -0800

    Attached PlayerController script to Player object; Added some basic debug outputs to PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private int frameCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player Controller Initialized...");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(frameCount);
+
+        if (frameCount >= 1000) {
+            frameCount = 0;
+            Debug.Log("Frame Counter Reset");
+        } else {
+            frameCount += 1;
+        }
     }
 }
