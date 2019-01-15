6db1526acf43fa7166f2180a25eff1b281a4e4b3
Arjay Jones <rj.javadev@icloud.com>
2019-01-15 14:37:48 -0800

    Added basic forward acceleration logic for laser bolts

---
+++ Assets/Scripts/Player/LaserBoltController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBoltController : MonoBehaviour
+{
+    [SerializeField]
+    private float laserBoltSpeed = 6.00f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Accelerates each laserBolt instance forward along Y axis
+        transform.Translate(Vector2.up * Time.deltaTime * this.laserBoltSpeed);
+    }
+}
