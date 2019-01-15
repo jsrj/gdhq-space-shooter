75731da331c481d3dec900e38160bbf1049471f0
Arjay Jones <rj.javadev@icloud.com>
2019-01-14 19:17:18 -0800

    Added user input to PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float x = 0.0f;
-    public float y = -3.85f;
-    public float z = 0.0f;
-    private Vector3 pos;
+    
+    private float startX = 0.0f;
+    private float startY = -3.85f;
+    private float startZ = 0.0f;
+
+    public float topBuffer = -2.45f;
+    public float verticalBounds = 3.85f;
+    public float horizontalBounds = 9.25f;
+    public float accelerationMultiplier = 0.15f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
         Debug.Log(this.name + " Initialized...");
-        transform.position = new Vector3(this.x, this.y, this.z);
+        transform.position = new Vector3(this.startX, this.startY, this.startZ);
     }
 
+    void FixedUpdate()
+    {
+
+        // move ship forward
+        if (Input.GetKey(KeyCode.W) && transform.position.y <= verticalBounds+topBuffer) {
+            transform.Translate(Vector2.up * accelerationMultiplier);
+        }
+
+        // move ship backward
+        if (Input.GetKey(KeyCode.S) && transform.position.y >= -verticalBounds) {
+            transform.Translate(Vector2.down * accelerationMultiplier);
+        }
+
+        // move ship left
+        if (Input.GetKey(KeyCode.A) && transform.position.x >= -horizontalBounds) {
+            transform.Translate(Vector2.left * accelerationMultiplier);
+        }
+
+        // move ship right
+        if (Input.GetKey(KeyCode.D) && transform.position.x <= horizontalBounds) {
+            transform.Translate(Vector2.right * accelerationMultiplier);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(this.x, this.y, this.z);
+
     }
 }
