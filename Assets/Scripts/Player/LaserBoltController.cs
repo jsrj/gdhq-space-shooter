74d13cad6b28ece1579874572cdbc672071cf25c
Arjay Jones <rj.javadev@icloud.com>
2019-01-16 22:35:36 -0800

    cleaned up code

--- Assets/Scripts/Player/LaserBoltController.cs
+++ Assets/Scripts/Player/LaserBoltController.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class LaserBoltController : MonoBehaviour
 {
     [SerializeField]
-    private float laserBoltSpeed;
+    private float _laserBoltSpeed;
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        this.laserBoltSpeed = 20.00f;
+
+        this._laserBoltSpeed = 20.00f;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        
         // Accelerates each laserBolt instance forward along Y axis
-        transform.Translate(Vector2.up * Time.deltaTime * this.laserBoltSpeed);
+        transform.Translate(Vector2.up * Time.deltaTime * this._laserBoltSpeed);
 
         // Destroy a laser bolt once it has moved a certain range out of camera frustrum
         if (transform.position.y > 10.00f) {
