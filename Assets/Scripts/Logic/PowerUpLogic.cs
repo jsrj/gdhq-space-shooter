ea1174ed13007fff4b1214ca486cadf0b70e6178
Arjay Jones <rj.javadev@icloud.com>
2019-01-17 22:31:30 -0800

    simplified logic to debug powerup getting stuck or not drifting

--- Assets/Scripts/Logic/PowerUpLogic.cs
+++ Assets/Scripts/Logic/PowerUpLogic.cs
@@ -15,7 +15,10 @@ public class PowerUpLogic : MonoBehaviour
     private float _driftRange;
 
     [SerializeField]
-    private float pos;
+    private string driftDirection;
+
+    [SerializeField]
+    private float _posx;
 
     // Start is called before the first frame update
     void Start()
@@ -30,45 +33,37 @@ public class PowerUpLogic : MonoBehaviour
     void Update()
     {
         // Update value of pos (position on x axis)
-        this.pos = transform.position.x;
+        this._posx = transform.position.x;
 
-        // clip fractional position value on edges of drift range
-        if (this.pos < -this._driftRange) {
-            this.pos = -this._driftRange;
-        }
-        
-        if (this.pos > this._driftRange) {
-            this.pos = this._driftRange;
-        }
-    
         // Move power up toward bottom of screen
         transform.Translate(Vector2.down * this._descendSpeed * Time.deltaTime);
+        
+        // clip fractional position value on edges of drift range
+        if (this._posx < -this._driftRange) {
+            this._posx = -this._driftRange;
+        }
+
+        if (this._posx > this._driftRange) {
+            this._posx = this._driftRange;
+        }
+    
 
         // Move power up left and right as it descends
-        // IF x position is less than 0 and greater than negative drift range
+        // IF x position is at max drift range or greater than negative drift range
         // THEN move left
-        if (transform.position.x < 0 && transform.position.x > -this._driftRange) {
+        if (this._posx > -this._driftRange || this._posx == this._driftRange) {
 
             transform.Translate(Vector2.left * this._driftSpeed * Time.deltaTime);
         }
 
-        // IF x position is greater than 0 OR less than negative drift range, and less than drift range 
+        // IF x position is at max negative drift range or less than drift range
         // THEN move right
-        if (transform.position.x > 0 && transform.position.x < this._driftRange) {
+        if (this._posx < this._driftRange || this._posx == -this._driftRange) {
 
             transform.Translate(Vector2.right * this._driftSpeed * Time.deltaTime);
         }
 
         // IF x position is exactly 0
         // THEN randomly choose a drift direction
-        if (transform.position.x == 0) {
-
-            if (Random.Range(0.00f, 1.00f) % 0.20f == 0) {
-                // Move left
-            } else {
-                // Move right
-            }
-        }
-
     }
 }
